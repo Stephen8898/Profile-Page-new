3a5a939e770c3843b30d3a91c04ab85d10608d20
Stephen Sarfo <Stephen.sarfo1998@outlook.com>
2018-10-14 22:00:23 -0400

    added a footer jsx and css file to the components folder

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
-
-export default class Contact extends Component {
+export default class Footer extends Component {
   render() {
     return (
       <div>
