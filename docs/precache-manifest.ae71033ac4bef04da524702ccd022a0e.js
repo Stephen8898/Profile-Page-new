8e2e06e05192cfc12e9c7046b1c5e53dc889691a
Stephen Sarfo <Stephen.sarfo1998@outlook.com>
2018-10-15 21:07:42 -0400

    Added https://stephen8898.github.io/Profile-Page-new/ to homepage in package.json then executed run build

---
+++ docs/precache-manifest.ae71033ac4bef04da524702ccd022a0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e43fb0ab9054245eea0",
+    "url": "/Profile-Page-new/static/js/runtime~main.4e43fb0a.js"
+  },
+  {
+    "revision": "c8f7982cc41d1240d7bd",
+    "url": "/Profile-Page-new/static/js/main.c8f7982c.chunk.js"
+  },
+  {
+    "revision": "e5ca5400469ba19c3747",
+    "url": "/Profile-Page-new/static/js/1.e5ca5400.chunk.js"
+  },
+  {
+    "revision": "c8f7982cc41d1240d7bd",
+    "url": "/Profile-Page-new/static/css/main.3eec23ea.chunk.css"
+  },
+  {
+    "revision": "a8a7bcc02f4d7161da36a65b5204f4e6",
+    "url": "/Profile-Page-new/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.582c4a5fbbaeb90d44748df443f9a0cf.js"
+  "/Profile-Page-new/precache-manifest.ae71033ac4bef04da524702ccd022a0e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/Profile-Page-new/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
