8dbf0ee7f1d9450c30d1f112203703c1c607729b
Stephen Sarfo <Stephen.sarfo1998@outlook.com>
2018-10-18 08:15:10 -0400

    made more changes  in the CostumeNavbar class to fix some bugs for the hashRouter.

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, NavItem} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { HashRouter} from  'react-router-dom';
 import './style/CustomNavbar.css';
 
 export default class CustomNavbar extends Component {
@@ -15,9 +16,9 @@ export default class CustomNavbar extends Component {
         <Navbar.Toggle/>
         <Navbar.Collapse>
         <Nav pullRight>
-              <NavItem eventKey={1}  href="/" >Home</NavItem>
-              <NavItem eventKey={2}  href="/about" >About</NavItem>
-              <NavItem eventKey={3}  href="/contact">Contact</NavItem>
+              <NavItem eventKey={1}  href="/" componentClass={Link} to="/">Home</NavItem>
+              <NavItem eventKey={2}  href="/about" componentClass={Link} to="/about" >About</NavItem>
+              <NavItem eventKey={3}  href="/contact" componentClass={Link} to="/contact">Contact</NavItem>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
