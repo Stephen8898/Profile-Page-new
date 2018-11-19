19b4839e59502bb331ca03f2168c5a7446c5dba9
Stephen Sarfo <Stephen.sarfo1998@outlook.com>
2018-11-18 19:08:19 -0500

    Updates

---
+++ precache-manifest.b505e10823027efe5c5f04c5c6be75ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c16cd90e5c623bf51220",
+    "url": "/static/js/main.c16cd90e.chunk.js"
+  },
+  {
+    "revision": "1d2cc60ac80046ad23b0",
+    "url": "/static/js/1.1d2cc60a.chunk.js"
+  },
+  {
+    "revision": "c16cd90e5c623bf51220",
+    "url": "/static/css/main.6e3eb897.chunk.css"
+  },
+  {
+    "revision": "6d048a19d95bae660674db15257fe883",
+    "url": "/index.html"
+  }
+];
